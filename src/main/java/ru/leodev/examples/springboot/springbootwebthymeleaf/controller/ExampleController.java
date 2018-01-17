40cba51a772ec02173e2587a6105ba9db1ac9088
leodev <leodestroy@gmail.com>
2018-01-18 02:49:35 +0300

    Complete SpringBootWebThymeleafApplication :beer:

---
+++ src/main/java/ru/leodev/examples/springboot/springbootwebthymeleaf/controller/ExampleController.java
@@ -0,0 +1,21 @@
+package ru.leodev.examples.springboot.springbootwebthymeleaf.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class ExampleController {
+    // с помощью этой аннотации значение из application.properties будет подставлено в поле message
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String home(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "home";
+    }
+
+}
