b6eb14cd99e650ef4e4d8055aee8b7e67e5ba951
leodev <leodestroy@gmail.com>
2018-01-18 01:01:55 +0300

    Init SpringBootWebThymeleafApplication :beer:

---
+++ src/main/java/ru/leodev/examples/springboot/springbootwebthymeleaf/SpringBootWebThymeleafApplication.java
@@ -0,0 +1,12 @@
+package ru.leodev.examples.springboot.springbootwebthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebThymeleafApplication.class, args);
+	}
+}
