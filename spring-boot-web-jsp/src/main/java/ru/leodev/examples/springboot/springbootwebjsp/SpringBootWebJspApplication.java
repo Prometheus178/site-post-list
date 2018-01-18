dd2f422a80e65430c33a767fe57ed0309ee51301
leodev <leodestroy@gmail.com>
2018-01-18 23:57:28 +0300

    Done Spring Boot Web JSP Example

--- spring-boot-web-jsp/src/main/java/ru/leodev/examples/springboot/springbootwebjsp/SpringBootWebJspApplication.java
+++ spring-boot-web-jsp/src/main/java/ru/leodev/examples/springboot/springbootwebjsp/SpringBootWebJspApplication.java
@@ -2,11 +2,18 @@ package ru.leodev.examples.springboot.springbootwebjsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebJspApplication {
+public class SpringBootWebJspApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebJspApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebJspApplication.class);
 	}
 }
