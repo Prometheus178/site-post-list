ff91dd5bd0059975017bbfb68b691f51cb9b96c4
leodev <leodestroy@gmail.com>
2018-01-20 03:54:34 +0300

    added spring-boot + slf4j

---
+++ spring-boot-web-thymeleaf-slf4j/src/main/java/ru/leodev/examples/springboot/slf4j/SpringBootWebThymeleafSLF4JApplication.java
@@ -0,0 +1,12 @@
+package ru.leodev.examples.springboot.slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebThymeleafSLF4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebThymeleafSLF4JApplication.class, args);
+	}
+}
