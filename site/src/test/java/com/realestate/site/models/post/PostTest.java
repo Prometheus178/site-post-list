eaf10e4e4c097324d4ef22006f123b1f69b70f5d
Sergey <siriusonestar@yandex.ru>
2019-08-01 22:29:23 +0300

    deployed

--- site/src/test/java/com/realestate/site/models/post/PostTest.java
+++ site/src/test/java/com/realestate/site/models/post/PostTest.java
@@ -7,14 +7,14 @@ import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.*;
 
-public class PostTest {
+//public class PostTest {
 
 
-    @Test
-    public void formattedDateTime() {
-        String dateTime = "2019-07-28T15:19:09.300";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String showTime = LocalDateTime.parse(dateTime).format(formatter);
-        System.out.println(showTime);
-    }
-}
+//    @Test
+//    public void formattedDateTime() {
+//        String dateTime = "2019-07-28T15:19:09.300";
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        String showTime = LocalDateTime.parse(dateTime).format(formatter);
+//        System.out.println(showTime);
+//    }
+//}
